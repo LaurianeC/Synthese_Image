4a2fbc3b8090d05c5f5e937fe4984a1c4dc479e4
Mathilde Bourjot <mathilde.bourjot@ensimag.grenoble-inp.fr>
2016-11-23 17:06:22 +0100

    a

--- SIA_TP/viewer/src/glshaderwindow.cpp
+++ SIA_TP/viewer/src/glshaderwindow.cpp
@@ -917,8 +917,8 @@ void glShaderWindow::render()
         lightCoordMatrix.lookAt(lightPosition, center, QVector3D(0,1,0)) ; 
 	lightPerspective.setToIdentity() ;
 	float radius = modelMesh->bsphere.r ; 
-
-        lightPerspective.perspective(45.0f, 1.0f, 2 * radius, 10 * radius);
+	//lightPerspective.perspective(45.0f,1.0f, (lightDistance-1.0) * modelMesh->bsphere.r, (lightDistance+1.0) * modelMesh->bsphere.r); 
+        lightPerspective.perspective(45.0f, 1.0f, 2.0 * radius, 10.0 * radius);
         shadowMapGenerationProgram->setUniformValue("matrix", lightCoordMatrix);
         shadowMapGenerationProgram->setUniformValue("perspective", lightPerspective);
 
